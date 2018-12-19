8eef8767d0ba18f2b0f504ce23a84ba7fa5069d7
SHAJ <shakilahmedshaj@gmail.com>
2018-12-19 14:35:07 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/sqlite_crud_student_details/MainActivity.java
@@ -0,0 +1,13 @@
+package com.t3ch.shaj.sqlite_crud_student_details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
